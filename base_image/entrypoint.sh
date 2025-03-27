2411e6941f324cf84a06fabc1a93d1a09be7af36
ijikeman <keiji.ue@gmail.com>
2025-03-28 00:49:01 +0900

    w

--- base_image/entrypoint.sh
+++ base_image/entrypoint.sh
@@ -7,19 +7,16 @@ echo "::endgroup::"
 
 # Install compile packages
 echo "::group::Install compile packages"
-bash /qmk_firmware/util/qmk_install.sh
-echo "::endgroup::"
-
-echo "::group::Debig"
-ls -al "$GITHUB_WORKSPACE/keyboards/"
+cd /qmk_firmware/
+bash util/qmk_install.sh
 echo "::endgroup::"
 
 # Copy keyboard files from workspace
 echo "::group::Copying keyboard files from workspace"
 if [[ -d "keyboards/${keyboard}" ]]; then
-    rm -rf /qmk_firmware/keyboards
+    rm -rf keyboards/${keyboard}
 fi
-cp -Rp "${GITHUB_WORKSPACE}/keyboards" "/qmk_firmware/"
+cp -Rp "${GITHUB_WORKSPACE}/keyboards/${keyboard}" "keyboards/"
 echo "::endgroup::"
 
 # Compile firmware
@@ -28,7 +25,7 @@ qmk compile -kb "${keyboard}/${rev}" -km "${keymap}"
 echo "::endgroup::"
 
 # Find compiled .hex file
-output_file=$(find /qmk_firmware -maxdepth 1 -name "*.hex" | head -n 1)
+output_file=$(find . -maxdepth 1 -name "*.hex" | head -n 1)
 
 # Save compiled .hex file
 if [[ -f "$output_file" ]]; then
